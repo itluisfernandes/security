0087c7bf73c346293fda688101ccb465e8f08ae5
Luis Fernandes <fernandes.luis@outlook.com>
2024-09-15 17:30:46 -0300

    Test

---
+++ Local_Admin_User_Audit.ps1
@@ -0,0 +1,39 @@
+﻿# Get Hostname
+$hostname = $env:COMPUTERNAME
+
+# Get time and date
+$currentDateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+
+# Check system language
+$language = (Get-Culture).Name
+
+# Set output file path and name
+$outputFile = "$hostname $currentDateTime.txt"
+
+# Check Administrators group name based on language - Supports Brazilian Portuguese and English.
+if ($language -eq "en-US") {
+    $administratorsGroup = "Administrators"
+}
+elseif ($language -eq "pt-BR") {
+    $administratorsGroup = "Administradores"
+}
+else {
+    Write-Host "Language not supported yet."
+    exit
+}
+
+# Get the members of the Administrators group
+$administrators = Get-LocalGroupMember -Group $administratorsGroup
+
+# Convert members to a formatted list
+$membersList = $administrators | Select-Object -ExpandProperty Name
+
+# Save the member list to a TXT file
+$membersList | Out-File -FilePath $outputFile
+
+# Copy file to remote server
+$remotePath = "\\REMOTESERVER\Folder\$outputFile"
+Copy-Item -Path $outputFile -Destination $remotePath
+
+# Remove local file
+Remove-Item -Path $outputFile
---
+++ README.md
@@ -0,0 +1,5 @@
+# Local Admin Users Audit - Security
+This script is used to help the infrastructure teams to obtain local admins users from servers or workstations to audit purposes.
+
+# How to use it?
+- In update
